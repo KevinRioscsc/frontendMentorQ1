9f105838663d56fadafb57dc3dc65142c703203a
Kevin Rios <rioskevin89@gmail.com>
2023-02-22 18:48:21 -0800

    first commit

---
+++ src/Components/QR.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import img from '../images/image-qr-code.png'
+import './style.css'
+
+const QR = () => {
+  return (
+    <div>
+        <div className="mainCard">
+           <img src={img} className='qrCard' alt="photo" />
+            <div className="words">
+                <p className="title"> Improve your front-end skills by building projects</p>
+                <p className="descr">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default QR
